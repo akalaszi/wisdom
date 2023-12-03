d1443f7badf720aaf6add9e4328688eff7b25a71
Adrian Kalaszi <kalaszi.adrian@dkatalis.com>
2023-12-03 11:39:48 +0100

    fix caching and update log messages for OMDB client

--- src/etl/OMDB.ts
+++ src/etl/OMDB.ts
@@ -9,19 +9,24 @@ const visited = new Set<string>();
 export async function fetchMovie(title: string): Promise<DataFile[]> {
   try {
     const cleanedTitle = title.trim().toLowerCase();
-
-    if (visited.size == 0 || visited.has(cleanedTitle)) {
+    if (cleanedTitle.length == 0 ) {
       logger.info(
-        `OMDB fetch is skiped for ${cleanedTitle} as it has been fetched before.`
+        `OMDB fetch is skiped for empty title.`
+      );
+      return Promise.resolve([]);
+    }
+    if (visited.has(cleanedTitle)) {
+      logger.info(
+        `OMDB fetch is skiped for the movie title: \"${cleanedTitle}\" as it has been fetched before.`
       );
       return Promise.resolve([]);
     }
 
-    logger.info(`Fetching data for ${cleanedTitle}`);
+    logger.info(`OMDB fetch for the movie title: \"${cleanedTitle}\"`);
     const response = await axios.get(URI + cleanedTitle);
     const content = JSON.stringify(response.data).split("\n");
     visited.add(title);
-    logger.info(`Fetched data for ${cleanedTitle}`);
+    logger.info(`OMDB fetch done for the movie title: \"${cleanedTitle}\"`);
     return Promise.resolve([new DataFile(cleanedTitle, content)]);
   } catch (error) {
     logger.error(`Error fetching data: ${error}`);
