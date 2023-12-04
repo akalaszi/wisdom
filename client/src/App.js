b09963bc5a64c6cf10c23d64d577c6ec51d00216
Adrian Kalaszi <kalaszi.adrian@dkatalis.com>
2023-12-04 09:12:41 +0100

    pagination works

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Page from "./Page";
-import {
-  BrowserRouter as Router,
-  useNavigate,
-  useLocation,
-} from "react-router-dom";
+let pageNumber = 0;
 
 function SearchPage() {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
-  const navigate = useNavigate();
-  const location = useLocation();
 
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const q = params.get("q");
-    if (q) {
-      setQuery(decodeURIComponent(q));
-    }
-  }, [location]);
+  const  handlePageIncrement = async () => {
+    console.log("before increment :" + pageNumber)
+    pageNumber = pageNumber + 1;
+    console.log("after increment :" + pageNumber)
+    await search()
+  };
+
+  const handlePageDecrement = async () => {
+    console.log("before deccrement :" + pageNumber)
+    pageNumber = pageNumber - 1;
+    console.log("after decrement :" + pageNumber)
+    await search()
+  };
 
   const search = async (event) => {
-    event.preventDefault();
-    const newResults = await fetch(`/search?q=${query}`).then((res) =>
+    if (event) {
+      event.preventDefault();
+    }
+    console.log("before search :" + pageNumber)
+    const newResults = await fetch(`/search?q=${query}&pageNumber=${pageNumber}`).then((res) =>
       res.json()
     );
     setResults(newResults);
-    navigate(`/?q=${encodeURIComponent(query)}`);
   };
 
   return (
-    <Router>
-      <div>
-        <form onSubmit={search}>
-          <input
-            type="text"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <button type="submit">Search</button>
-        </form>
-        {<Page result={results} query={query} />}
-      </div>
-    </Router>
+    <div>
+      <form onSubmit={search}>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {<Page result={results} onIncrement={handlePageIncrement} onDecrement={handlePageDecrement} />}
+    </div>
   );
 }
 
--- client/src/Page.js
+++ client/src/Page.js
@@ -1,29 +1,25 @@
-function Page({ result, query }) {
+function Page({ result, onIncrement, onDecrement }) {
   if (!result.results) {
     return null;
   }
-  const handlePrevious = () => {
-    console.log(query);
-  };
-  const handleNext = () => {
-    console.log(query);
-  };
+
   return (
     <div>
       <p></p>
       {result.results.map((r, index) => (
         <div key={index}>
           <div>{r.summary}</div>
-          <a href={r.uri}>{r.uri}</a>
+          <a href={r.uri} target="_blank" rel="noopener noreferrer">
+            {r.uri}
+          </a>
           <p></p>
         </div>
       ))}
 
-      {result.pageNumber > 0 && (
-        <button onClick={handlePrevious}>Previous</button>
-      )}
+      {result.pageNumber > 0 && <button onClick={onDecrement}>Previous</button>}
       <div>total hits: {result.totalResults}</div>
-      {result.nextPageSize > 0 && <button onClick={handleNext}>Next</button>}
+      <div>current page: {result.pageNumber}</div>
+      {result.nextPageSize > 0 && <button onClick={onIncrement}>Next</button>}
     </div>
   );
 }
